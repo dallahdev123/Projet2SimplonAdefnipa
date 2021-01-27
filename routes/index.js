42df9c9008167217588c03cacf695807ffe1fd2a
sawalo <basandjiry7@gmail.com>
2021-01-27 16:51:21 +0000

    insertion d'image background

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var con = mysql.createConnection({
     host: "localhost",
-    user: "dallah",
-    password: "passer",
+    user: "root",
+    password: "present",
     database: "meteo_db"
   });
 
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    var img = 'INSERT INTO image(name_img) VALUES("images/mi.jpg")';
+    var img = 'INSERT INTO image(name_img) VALUES("images/ma.jpg")';
+    var img = 'INSERT INTO image(name_img) VALUES("images/ciel.jpg")';
+    var img = 'INSERT INTO image(name_img) VALUES("images/nuanges.jpg")';
+
     var sql = "INSERT INTO tmp_humidity (temperature, humidite) VALUES ('30','55')";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
+    });
+    con.query(img, function(err, result){
+      if (err) throw err;
+      console.log(' insertion');
     });
   });
 
